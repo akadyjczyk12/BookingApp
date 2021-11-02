17728dc21ffe3641492d772b1691ab2885d40846
MarcinJen <martos19993@gmail.com>
2021-11-02 15:42:33 +0100

    unsubscribe

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,25 +8,30 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent implements OnInit, OnDestroy {
+  private sub: Subscription = new Subscription();
   public model = {
     username: '',
     password: ''
   };
 
   constructor(private authService: AuthService, private router: Router) { }
+  
+  ngOnDestroy(): void {
+    if (this.sub)
+      this.sub.unsubscribe()
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    this.authService.login(this.model).subscribe();
-    this.authService.userData$.subscribe(user => {
+    this.sub.add(this.authService.login(this.model).subscribe());
+    this.sub.add(this.authService.userData$.subscribe(user => {
       if (user) {
         this.router.navigate(['dashboard']);
       }
-    });
+    }));
 
   }
 
